eef5a7f2cafa614653a673aa54da8f8e3c16976d
celiaxiao <c6xiao@ucsd.edu>
2021-08-27 19:29:35 +0800

    implement: docker for api-gateway and auth-server(not working)

---
+++ docker/docker-auth-server/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+source ../../resources/auth.env
+
+export APP_IMAGE_NAME=${APP_IMAGE_NAME}
+export APP_IMAGE_VERSION=${APP_IMAGE_VERSION}
+export CONTAINER_NAME=${CONTAINER_NAME}
+export CONTAINER_PORT=${CONTAINER_PORT}
+export VOLUME_CONF_EXT=${VOLUME_CONF_EXT}
+export VOLUME_CONF_INN=${VOLUME_CONF_INN}
+export ENV_PATH=${ENV_PATH}
+
+
+# echo `ls $VOLUME_CONF_EXT`
+docker-compose down
+docker-compose up 
