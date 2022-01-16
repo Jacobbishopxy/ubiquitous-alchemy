1d8af3046e1cf535e56aaba50e520cab2a6d0a4d
Jacob <jacobbishopxy@gmail.com>
2022-01-16 12:23:03 +0800

    server-nodejs rm cryptoes

--- server-nodejs/tests/crypto_test.js
+++ server-nodejs/tests/crypto_test.js
@@ -29,39 +29,6 @@ const aes256gcm = (key) => {
   }
 }
 
-
-function toUTF8Array(str) {
-  let utf8 = []
-  for (let i = 0; i < str.length; i++) {
-    let charcode = str.charCodeAt(i)
-    if (charcode < 0x80) utf8.push(charcode)
-    else if (charcode < 0x800) {
-      utf8.push(0xc0 | (charcode >> 6),
-        0x80 | (charcode & 0x3f))
-    }
-    else if (charcode < 0xd800 || charcode >= 0xe000) {
-      utf8.push(0xe0 | (charcode >> 12),
-        0x80 | ((charcode >> 6) & 0x3f),
-        0x80 | (charcode & 0x3f))
-    }
-    // surrogate pair
-    else {
-      i++
-      // UTF-16 encodes 0x10000-0x10FFFF by
-      // subtracting 0x10000 and splitting the
-      // 20 bits of 0x0-0xFFFFF into two halves
-      charcode = 0x10000 + (((charcode & 0x3ff) << 10)
-        | (str.charCodeAt(i) & 0x3ff))
-      utf8.push(0xf0 | (charcode >> 18),
-        0x80 | ((charcode >> 12) & 0x3f),
-        0x80 | ((charcode >> 6) & 0x3f),
-        0x80 | (charcode & 0x3f))
-    }
-  }
-  return utf8
-}
-
-
 function str2ab(str) {
   var buf = new ArrayBuffer(str.length * 2) // 2 bytes for each char
   var bufView = new Uint16Array(buf)
@@ -77,8 +44,8 @@ const cipher = aes256gcm(Buffer.alloc(32, key))
 
 {
   const msg = "{\"email\":\"jacob@example.com\",\"nickname\":\"Jacob\"}"
-  const ct = cipher.encrypt(msg)
 
+  const ct = cipher.encrypt(msg)
   console.log(ct.toString('base64'))
 
   const pt = cipher.decrypt(ct)
