ad4b43972e92d9082ce6cab19266f12985a1a7b6
Jacob <jacobbishopxy@gmail.com>
2022-02-22 00:49:04 +0800

    chore

--- docker-app/asset-management/setup.sh
+++ docker-app/asset-management/setup.sh
@@ -2,6 +2,18 @@
 # author: Jacob Bishop
 
 source ../../resources/java.env
+
+# name builder image
+docker build \
+		--target builder \
+    -t "${AM_CACHE_NAME}":"${AM_CACHE_VERSION}" \
+    --build-arg MAVEN_IMAGE_NAME="${MAVEN_IMAGE_NAME}" \
+    --build-arg MAVEN_IMAGE_VERSION="${MAVEN_IMAGE_VERSION}" \
+    --build-arg JAVA_BASE_NAME="${JAVA_BASE_NAME}" \
+    --build-arg JAVA_BASE_VERSION="${JAVA_BASE_VERSION}" \
+    -f ./Dockerfile ../..
+
+# build app image
 docker build \
 		-t "${AM_BASE_NAME}":"${AM_BASE_VERSION}" \
     --build-arg MAVEN_IMAGE_NAME="${MAVEN_IMAGE_NAME}" \
